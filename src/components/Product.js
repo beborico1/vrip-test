a84c424d025c6a9f1e9a00f13ed5fcf508e17398
beborico1 <beborico16@hotmail.com>
2022-10-04 20:46:47 -0500

    improved product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Product({product}) {
+  const [image,setImage] = useState(null)
+
+  useEffect(() => {
+    let img = product.data().images[0]
+    product.data().images.map(image => {
+        if (image.includes("Front")) {
+          img = image
+        }
+    })
+
+    setImage(img)
+  },[])
+
   return (
-    <div className="rounded-lg mr-4 mb-4 bg-white shadow-md px-7 py-5 content-center">
-        <div className='flex justify-center'>
-            <img className="object-contain" alt="Product" src={product.data().images[0]}/>                    
+    <div className="rounded-lg mr-4 mb-4 bg-white shadow-md content-center">
+        <div className='flex justify-center w-full'>
+            <img className="rounded-lg object-contain w-full" alt="Product" src={image}/>                    
         </div>
-        <div className="mt-3">
-            <p className='font-bold text-center line-clamp-1'>{product.data().name}</p>
-            <p className='text-center text-sm text-gray-500 line-clamp-3 mt-3'>{product.data().description}</p>
+        <div className="px-4 mb-4 mt-1">
+            <p className='font-bold text-center text-xs'>{product.data().name}</p>
         </div>
     </div>
   )
