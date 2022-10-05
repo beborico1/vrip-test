8aee79c1e4c74d5a3ab41f033d3da7590b2b035a
beborico1 <beborico16@hotmail.com>
2022-10-04 21:49:20 -0500

    introduced front_image

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 function Product({product}) {
-  const [image,setImage] = useState(null)
-
-  useEffect(() => {
-    let img = product.data().images[0]
-    product.data().images.map(image => {
-        if (image.includes("Front")) {
-          img = image
-        }
-    })
-
-    setImage(img)
-  },[])
-
   return (
     <div className="rounded-lg mr-4 mb-4 bg-white shadow-md content-center">
         <div className='flex justify-center w-full'>
             <img className="rounded-lg object-contain w-full" alt="Product" src={image}/>                    
         </div>
         <div className="px-4 mb-4 mt-1">
-            <p className='font-bold text-center text-xs'>{product.data().name}</p>
+            <p className='font-bold text-center text-xs'>{product.data().front_image}</p>
         </div>
     </div>
   )
