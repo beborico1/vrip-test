4dc6f0ac22252d478adbc8439473ac42808ad283
beborico1 <beborico16@hotmail.com>
2022-10-06 16:34:52 -0500

    toggle details

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { db } from '../firebase-config'
 import Video from 'react-responsive-video'
-
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css';
 
@@ -11,6 +10,7 @@ function ProductDetail() {
     const {brand_id,product_id} = useParams()
     const [product,setProduct] = useState({})
     const [images,setImages] = useState([])
+    const [moreInfo,setMoreInfo] = useState(false)
     
     const productRef = doc(db,`products/${product_id}`)
 
@@ -34,52 +34,57 @@ function ProductDetail() {
     return (
         <div>
             <div className='m-3 p-5 bg-white rounded shadow-md'>
-                <Link to={`/${brand_id}`}><b className='hover:text-gray-700'>{"Go back"}</b></Link>
+                <Link to={`/${brand_id}`}><b className='hover:text-gray-700 mb-5'>{"Go back"}</b></Link>
+                <div className='mb-5 mt-5'>
+                    <Slide>
+                        {images.map(image => (
+                            <div className="">
+                                <img className='object-contain h-80 w-full' src={image}/>
+                            </div>
+                        ))}
+                        
+                    </Slide>
+                </div>
+
                 <div className='flex flex-col items-center space-y-3'>
-                    {product.video_src==="" ? (<div></div>):(<div>
-                        <Video className="w-60" mp4={product.video_src}/>
-                    </div>)}
                     
                     <h1 className='font-bold'>{product.name}</h1>
-                    <p className='mb-5 text-center text-sm text-gray-700'>{product.description}</p>
-                    <div className='h-full w-full'>
-                        <Slide className="">
-                            {images.map(image => (
-                                <div className='bg-red' key={image}>  
-                                    <img className='object-cover w-full' src={image}/>
-                                    <p className='text-white line-clamp-1'>{image}</p>
-                                </div>
-                            ))}
-                        </Slide>
+                    <div className='text-center text-sm text-gray-700 mt-6 mb-5'>
+                        <p className='mb-5 text-center text-sm text-gray-700'>{product.description}</p>
+
+                        {product.video_src==="" ? (<div></div>):(<div className='flex flex-col items-center mb-5'>
+                            <Video className="h-96" mp4={product.video_src}/>
+                        </div>)}
+
+                        <button onClick={() => setMoreInfo(!moreInfo)} className='mb-2 font-bold text-black hover:text-gray-800'>{moreInfo ? "- hide details":"+ see details"}</button>
+                        
+                        {moreInfo ? (<div> 
+                            <b className=''>By:</b>
+                            <span className=''> {product.brand_name}</span>
+                            <br/>
+                            <b className=''>User:</b>
+                            <span className=''> @{product.brand_id}</span>
+                            <br/>
+                            <b className=''>SKU:</b>
+                            <span className=''> {product.inside_sku}</span>
+                            <br/>
+                            <b className=''>ID:</b>
+                            <span className=''> {product.inside_id}</span>
+                            <br/>
+                            <b className=''>Gender:</b>
+                            <span className=''> {product.gender}</span>
+                            <br/>
+                            <b className=''>Category:</b>
+                            <span className=''> {product.category}</span>
+                            <br/>
+                            <b className=''>Available Online:</b>
+                            <span className=''> {product.availabe_online ? "Yes":"No"}</span>
+                        </div>):(<div></div>)}
                     </div>
                 </div>
-                <br/>
 
-                
-                <div className='text-center text-sm text-gray-700 mt-6 mb-5'>
-                        <b className=''>By:</b>
-                        <span className=''> {product.brand_name}</span>
-                        <br/>
-                        <b className=''>User:</b>
-                        <span className=''> @{product.brand_id}</span>
-                        <br/>
-                        <b className=''>SKU:</b>
-                        <span className=''> {product.inside_sku}</span>
-                        <br/>
-                        <b className=''>ID:</b>
-                        <span className=''> {product.inside_id}</span>
-                        <br/>
-                        <b className=''>Gender:</b>
-                        <span className=''> {product.gender}</span>
-                        <br/>
-                        <b className=''>Category:</b>
-                        <span className=''> {product.category}</span>
-                        <br/>
-                        <b className=''>Available Online:</b>
-                        <span className=''> {product.availabe_online ? "Yes":"No"}</span>
-                    </div>
-                
-                <p className='w-full text-center mb-4 font-medium'>$ {product.price_float} usd</p> 
+                                
+                <p className='w-full text-center mb-4 mt-4 font-medium'>$ {product.price_float} usd</p> 
 
                 <div className='flex justify-center'>
                     <a className='bg-black text-white rounded-lg px-4 py-1' href={product.buy_url}>Buy</a>
