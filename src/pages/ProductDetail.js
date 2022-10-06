3d9f76b4ccc966fcb1fc9d80f4e197653bf26f07
beborico1 <beborico16@hotmail.com>
2022-10-06 17:26:53 -0500

    add red bg

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -35,11 +35,11 @@ function ProductDetail() {
         <div>
             <div className='m-3 p-5 bg-white rounded shadow-md'>
                 <Link to={`/${brand_id}`}><b className='hover:text-gray-700 mb-5'>{"Go back"}</b></Link>
-                <div className='my-5'>
-                    <Slide>
+                <div className='my-5 bg-red-400'>
+                    <Slide className="bg-red-300">
                         {images.map(image => (
-                            <div className="w-full flex flex-col items-center">
-                                <img className='object-cover h-80 w-80' src={image}/>
+                            <div className="w-full flex flex-col items-center bg-red-200">
+                                <img className='object-cover h-80 w-80 bg-red-100' src={image}/>
                             </div>
                         ))}
                         
