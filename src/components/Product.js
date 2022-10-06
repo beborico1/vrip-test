1bec5bbaf2453b6b4002ada6abe1e81e6d857be4
beborico1 <beborico16@hotmail.com>
2022-10-05 21:25:04 -0500

    same item height

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom'
 function Product({product}) {
   return (
     <Link to={`/${product.data().brand_id}/${product.id}`}>
-      <div className="rounded-lg pb-3 m-1 bg-white shadow-md content-center">
-          <div className='flex justify-center w-full'>
-              <img className="rounded-lg object-contain w-full" alt="Product" src={product.data().front_image}/>                    
-          </div>
-          <div className="px-4">
-              <p className='font-bold text-center text-xs'>{product.data().name}</p>
-          </div>
+      <div className='p-1 h-full'>
+        <div className="rounded-lg pb-3  bg-white shadow-md content-center h-full">
+            <div className='flex justify-center w-full'>
+                <img className="rounded-lg object-contain w-full" alt="Product" src={product.data().front_image}/>                    
+            </div>
+            <div className="px-4">
+                <p className='font-bold text-center text-xs'>{product.data().name}</p>
+            </div>
+        </div>
       </div>
     </Link>
   )
