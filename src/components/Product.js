77ddf6cf2ae12e203423f258bf1594be0be4430d
beborico1 <beborico16@hotmail.com>
2022-10-12 14:44:22 -0500

    added gray bg

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ function Product({product}) {
   return (
     <Link to={`/${product.data().brand_id}/${product.id}`}>
       <div className='h-full'>
-        <div className="p-1 h-full">
+        <div className="p-1 h-full bg-gray-300">
             <div className='flex justify-center w-full'>
                 <img className="object-contain w-full" alt="Product" src={product.data().front_image}/>                    
             </div>
